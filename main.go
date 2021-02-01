7835293d50e0c85458e40e254496150ef8d5d6c7
Assaf akiva <assaf.akiva@oracle.com>
2021-02-01 11:31:49 +0200

    splitted into 2 channels

--- main.go
+++ main.go
@@ -10,15 +10,15 @@ func main() {
 	f, _ := os.Open("sample1.wav")
 	f2, _ := os.Open("sample3.wav")
 
-	s1, _, _ := wav.Decode(f)
+	s1, format, _ := wav.Decode(f)
 	s2, _, _ := wav.Decode(f2)
 
-	mixedStream := beep.Mix(s1, s2)
-	format := beep.Format{
-		SampleRate:  44100,
-		NumChannels: 2,
-		Precision:   2,
-	}
+	//s1.Seek(50000)
+
+	leftCh := multiplyChannels(1, 0, s1)
+	rightCh := multiplyChannels(0, 1, s2)
+
+	mixedStream := beep.Mix(leftCh, rightCh)
 
 	f, err := os.Create("mixed.wav")
 	if err != nil {
@@ -29,3 +29,18 @@ func main() {
 
 	wav.Encode(f, mixedStream, format)
 }
+
+func multiplyChannels(left, right float64, s beep.Streamer) beep.Streamer {
+	return beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
+		n, ok = s.Stream(samples)
+		for i := range samples[:n] {
+			samples[i][0] *= left
+			samples[i][1] *= right
+		}
+		return n, ok
+	})
+}
+
+//func samplesFromTime(t time.Duration, ) int {
+//	return int(t * time.Duration(sr) / time.Second)
+//}
